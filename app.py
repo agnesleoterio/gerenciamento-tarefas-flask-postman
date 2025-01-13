1feabca666ecce6f5ffb897fd2200c16c7201a28
Agnes <agnes.leoterio.alves@gmail.com>
2025-01-13 16:17:23 +0000

    adiciona funcionalidades de atualizar e deletar ao endpoint de /users

--- app.py
+++ app.py
@@ -21,6 +21,37 @@ def get_user(user_id):
             return jsonify(user)
     return jsonify({'message': 'User not found'}), 404
 
+# Endpoint para adicionar um novo usuário
+@app.route('/users', methods=['POST'])
+def add_user():
+    new_user = request.get_json()
+    if users:
+        new_user['id'] = users[-1]['id'] + 1
+    else:
+        new_user['id'] = 1
+    users.append(new_user)
+    return jsonify(new_user), 201
+
+# Endpoint para atualizar um usuário existente
+@app.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    for user in users:
+        if user['id'] == user_id:
+            data = request.get_json()
+            user.update(data)
+            return jsonify(user)
+    return jsonify({'message': 'User not found'}), 404
+
+# Endpoint para deletar um usuário
+@app.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    for user in users:
+        if user['id'] == user_id:
+            users.remove(user)
+            return jsonify({'message': 'User deleted'})
+    return jsonify({'message': 'User not found'}), 404
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
